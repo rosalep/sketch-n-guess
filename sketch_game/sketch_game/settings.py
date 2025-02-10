50b7a869f653795f7a5edd2a9dca468d1bfa0683
rosalep <111808082+rosalep@users.noreply.github.com>
2025-02-10 12:30:18 -0800

    SECRET_KEY removed
    Temporarily removed

--- sketch_game/sketch_game/settings.py
+++ sketch_game/sketch_game/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fnmayqi%mvqmyc5!aa8(v0g+hrog6orpbgfd$qu_awwdp)-y(o'
+# secret key goes here
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
