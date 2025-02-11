6a6211b68579359c4a88993e4859623ae3f39e69
rosalep <prosales@csuchico.edu>
2025-02-10 18:40:08 -0800

    SECRET_KEY changed and removed from settings.py

--- sketch_game/sketch_game/settings.py
+++ sketch_game/sketch_game/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os # helps with secrets
+from dotenv import load_dotenv # .env
+
+load_dotenv() # loads .env variables
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# secret key goes here
-
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
